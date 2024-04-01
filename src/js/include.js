15a31ffe42fc3aa3ce9f177a264baf40e31f8292
Lettuce <45330684+Lettuce144@users.noreply.github.com>
2024-04-01 14:46:32 +0200

    Header done

--- src/js/include.js
+++ src/js/include.js
@@ -2,37 +2,51 @@
 
 // Recursive function to process includes
 function processIncludes(element) {
-    const includeLocation = element.getAttribute("at");
-    const includeHtmlContent = element.getAttribute("html");
-  
-    fetch(includeHtmlContent)
-      .then(response => response.text())
-      .then(data => {
-        if (includeLocation !== null) {
-          // If 'at' attribute is present, replace the content of the target element
-          const locationElement = document.getElementById(includeLocation);
-          console.log("Processing include: " + locationElement);
-          locationElement.innerHTML = data;
-  
-          element.remove();
-          // Process any sub-includes in the fetched HTML
-          Array.from(locationElement.getElementsByTagName("include")).forEach(processIncludes);
-        } else {
-          // If 'at' attribute is not present, replace the <include> element itself
-          //Remove tempDiv?
-          const tempDiv = document.createElement('div');
-          tempDiv.innerHTML = data;
-          element.replaceWith(...tempDiv.childNodes);
-  
-          // Process any sub-includes in the new HTML
-          Array.from(tempDiv.getElementsByTagName("include")).forEach(processIncludes);
+  const includeLocation = element.getAttribute("at");
+  const includeHtmlContent = element.getAttribute("html");
+
+  fetch(includeHtmlContent)
+    .then((response) => response.text())
+    .then((data) => {
+      let locationElement;
+      if (includeLocation !== null) {
+        locationElement = document.getElementById(includeLocation);
+        console.log("Processing include: " + locationElement);
+        locationElement.innerHTML = data;
+        element.remove();
+      } else {
+        const tempDiv = document.createElement("div");
+        tempDiv.innerHTML = data;
+        element.replaceWith(...tempDiv.childNodes);
+        locationElement = tempDiv;
+      }
+
+      // Process any sub-includes in the fetched HTML
+      Array.from(locationElement.getElementsByTagName("include")).forEach(
+        processIncludes
+      );
+
+      // Execute any scripts in the fetched HTML
+      Array.from(locationElement.getElementsByTagName("script")).forEach(
+        (oldScript) => {
+          const newScript = document.createElement("script");
+          Array.from(oldScript.attributes).forEach((attr) =>
+            newScript.setAttribute(attr.name, attr.value)
+          );
+          if (oldScript.src) {
+            newScript.src = oldScript.src;
+          } else {
+            newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+          }
+          oldScript.parentNode.replaceChild(newScript, oldScript);
         }
-      });
-  }
-  
-  // Get all <include> tags
-  const includeElements = document.getElementsByTagName("include");
-  
-  // Iterate over all <include> tags
-  Array.from(includeElements).forEach(processIncludes);
-  
+      );
+    });
+}
+
+// Get all <include> tags
+const includeElements = document.getElementsByTagName("include");
+
+// Iterate over all <include> tags
+Array.from(includeElements).forEach(processIncludes);
+
